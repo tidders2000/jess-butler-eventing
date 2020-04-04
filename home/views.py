14f819c0e3e87f9c7fb5994896a87fed11eab857
Simon Tidman <simontidman@Simons-MBP.lan>
2020-04-04 11:45:24 +0100

    changed access token

--- home/views.py
+++ home/views.py
@@ -13,10 +13,10 @@ from .forms import signupform
 
 def index(request):
     
-    r = requests.get('https://graph.facebook.com/v4.0/me?fields=id%2Cname%2Cfeed&access_token=EAAFwJhMIpkUBAJZCGoIvHYcqeprUqWGEasJ2pPCjageosx35is5ZAH1gn4xnBhFoKqV6NzAhXn25xvIhYIXkTjODCeKy9FlQSGQX496ZAQ7MuN8FgWLNlTXh1vYBeyE0OtofnxKy80UMBay6aZCqkUCVZAlca1KE5hGaoaZA5q6pcJReZC6bL0eDzHPZCtPNf6ZBRrMmnTYvAxAZDZD')
+    r = requests.get('https://graph.facebook.com/v4.0/me?fields=id%2Cname%2Cfeed&access_token=EAAFwJhMIpkUBAB7RVKxV9UzCliSIGN8pBcxcfiTzByHu7o0yui9lJWmrNfnvdoKd0v5hQ6v8ZB2QTiZCr7yOETobw9f6rd5icrhuzOue2PStqyfk0V7ZCcUYHMaisdPslWs3lZB5ZAI68DdVt4fvG9k5y4keN0PY0F5X4YwYS6gZDZD)
     json = r.json()
     
-    h = requests.get('https://graph.facebook.com/v5.0/230231543752378?fields=feed.limit(9)%7Bmessage%2Cpermalink_url%2Cfull_picture%7D&access_token=EAAFwJhMIpkUBAI8drgzZAd3I8KntuoA6ZB9ZCTT3P1fjLkizWc4Tr3QbSqLHbGAIJZBVyIXOX1zfLXhHbkvYbnEROCSqaITuYh0IoPz7KDfDfuCZC9VqFViun185jcwPfPIZBZCHz9E1VPLXrnfgLJ70Igdez7AHqdgaiK574DuHAZDZD')
+    h = requests.get('https://graph.facebook.com/v5.0/230231543752378?fields=feed.limit(9)%7Bmessage%2Cpermalink_url%2Cfull_picture%7D&access_token=EAAFwJhMIpkUBAB7RVKxV9UzCliSIGN8pBcxcfiTzByHu7o0yui9lJWmrNfnvdoKd0v5hQ6v8ZB2QTiZCr7yOETobw9f6rd5icrhuzOue2PStqyfk0V7ZCcUYHMaisdPslWs3lZB5ZAI68DdVt4fvG9k5y4keN0PY0F5X4YwYS6gZDZD')
     json2 = h.json()
     fb=Feedback.objects.all()
     Signupform=signupform()
